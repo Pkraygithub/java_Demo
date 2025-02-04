4aa453578d6d4209c58976bc5260fd905df73141
Pkraygithub <pankaj9576.kr@gmail.com>
2025-02-04 21:02:14 +0530

    First Commit

---
+++ Java_Demo/src/JavaTest/java_Demo.java
@@ -0,0 +1,10 @@
+package JavaTest;
+
+public class java_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java_Demo/src/java_Demo.java
@@ -0,0 +1,14 @@
+public class java_Demo {
+
+	public static void main(String[] args) {
+
+
+		
+		int [] aa= {3,4,5,4,2,3};
+		
+		for(int aar: aa) {
+			System.out.println(aar);
+		}
+	}
+
+}
